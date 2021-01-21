bb728a7f74220bfd5ab392cf18e85dfbcae0a27e
AfricanBongo <tilormta@outlook.com>
2021-01-21 14:40:06 +0200

    Created custom navigation view and LocationsActivity

--- app/src/main/java/com/africanbongo/clearskyes/controller/activities/MainActivity.java
+++ app/src/main/java/com/africanbongo/clearskyes/controller/activities/MainActivity.java
@@ -4,6 +4,7 @@ import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -55,6 +56,7 @@ public class MainActivity extends AppCompatActivity
 
 
 
+        toggle.getDrawerArrowDrawable().setColor(Color.WHITE);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
